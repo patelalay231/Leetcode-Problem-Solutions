5414f83e9e9d4f637b4829756f9d55d53f425767
Alay <60166361+patelalay231@users.noreply.github.com>
2024-04-15 18:02:16 +0530

    Time: 164 ms (55.26%), Space: 89 MB (52.72%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,22 +1,17 @@
 class StockSpanner {
 public:
-    vector<int> prices;
+    stack<pair<int,int>> st;
     StockSpanner() {
         
     }
     
     int next(int price) {
         int cnt = 1;
-        prices.push_back(price);
-        int n = prices.size();
-        int i =0;
-        for(i=n-1;i>=0;i--){
-            if(prices[i] > price){
-                cnt = n-i-1;
-                break;
-            }
+        while(!st.empty() && st.top().first <= price){
+            cnt += st.top().second;
+            st.pop();
         }
-        if(i == -1) return n;
+        st.push({price,cnt});
         return cnt;
     }
 };
