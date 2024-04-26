bf1eb4cc694103ac4787bf5f6530ebe07407664b
Alay <60166361+patelalay231@users.noreply.github.com>
2024-04-26 23:27:50 +0530

    Time: 133 ms (69%), Space: 55.7 MB (15.5%) - LeetHub

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/1422-divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/1422-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     bool isPossibleDivide(vector<int>& hand, int groupSize) {
-        sort(hand.begin(),hand.end());
-        unordered_map<int,int> mp;
-        for(auto it:hand){
+        unordered_map<int, int> mp;
+        for (auto it : hand) {
             mp[it]++;
         }
         int n = hand.size();
-        if(n%groupSize != 0) return false;
-        for(int i=0;i<n;i++){
-            if(mp.find(hand[i]) != mp.end()){
-                for(int j=0;j<groupSize;j++){
-                    if(mp.find(hand[i]+j) == mp.end()){
-                        return false;
-                    }
-                    mp[hand[i]+j]--;
-                    if(mp[hand[i]+j] == 0){
-                        mp.erase(hand[i]+j);
-                    }
+        if (n % groupSize != 0)
+            return false;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (auto it : mp) {
+            pq.push(it.first);
+        }
+        while (!pq.empty()) {
+            int ele = pq.top();
+            for (int i = 0; i < groupSize; i++) {
+                if (mp.find(ele + i) == mp.end()) {
+                    return false;
+                }
+                mp[ele + i]--;
+                if (mp[ele + i] == 0) {
+                    pq.pop();
+                    mp.erase(ele + i);
                 }
             }
         }
