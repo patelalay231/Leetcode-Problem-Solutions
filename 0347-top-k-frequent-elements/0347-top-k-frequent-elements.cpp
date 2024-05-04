2d2f3d972f5d61ae92d3e239a48ba9ed697d0f3a
Alay <60166361+patelalay231@users.noreply.github.com>
2024-05-04 22:58:09 +0530

    Time: 4 ms (98.11%), Space: 17.2 MB (62.3%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -6,20 +6,41 @@ public:
         for (auto ele : nums) {
             mp[ele]++;
         } // o(n)
-        vector<pair<int, int>> freqMp;
-        for (auto it : mp) {
-            freqMp.push_back({it.second, it.first});
-        } // 0(klogk)
-        auto cmp = [](const pair<int, int>& a, const pair<int, int>& b) {
-        return a.first > b.first;
-        };
-        sort(freqMp.begin(), freqMp.end(), cmp);
+
+        // ----- approach 1 -----
+        
+        // vector<pair<int, int>> freqMp;
+        // for (auto it : mp) {
+        //     freqMp.push_back({it.second, it.first});
+        // } // 0(klogk)
+        // auto cmp = [](const pair<int, int>& a, const pair<int, int>& b) {
+        // return a.first > b.first;
+        // };
+        // sort(freqMp.begin(), freqMp.end(), cmp);
+
+        // vector<int> ans;
+        // for (auto it : freqMp) {
+        //     ans.push_back(it.second);
+        //     if (ans.size() == k)
+        //         break;
+        // } // 0(k)
+
+
+        // ----- approach 2 -----   
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+
+        for(auto it:mp){
+            pq.push({it.second,it.first});
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
         vector<int> ans;
-        for (auto it : freqMp) {
-            ans.push_back(it.second);
-            if (ans.size() == k)
-                break;
-        } // 0(k)
+        while(k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
         return ans;
     }
 };
