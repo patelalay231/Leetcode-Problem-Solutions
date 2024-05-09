b4c027e6bce518834bceb85c3d96d3c56a3f2ec6
Alay <60166361+patelalay231@users.noreply.github.com>
2024-05-09 19:54:45 +0530

    Time: 14 ms (34.04%), Space: 20.5 MB (27.77%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(),intervals.end());
-        int n = intervals.size();
-        vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
-        for(int i=1;i<n;i++){
-            if(ans.back()[1] >= intervals[i][0]){
-                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
-            }
-            else{
-                ans.push_back(intervals[i]);
-            }
-        }   
-        return ans;
+   vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n = intervals.size(), i = 0;
+        vector<vector<int>> res;
+        //case 1: no overlapping case before the merge intervals
+		//compare ending point of intervals to starting point of newInterval
+        while(i < n && intervals[i][1] < newInterval[0]){
+            res.push_back(intervals[i]);
+            i++;
+        }                           
+		//case 2: overlapping case and merging of intervals
+        while(i < n && newInterval[1] >= intervals[i][0]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.push_back(newInterval);
+        // case 3: no overlapping of intervals after newinterval being merged
+        while(i < n){
+            res.push_back(intervals[i]);
+            i++;
+        }
+        return res;
     }
 };
