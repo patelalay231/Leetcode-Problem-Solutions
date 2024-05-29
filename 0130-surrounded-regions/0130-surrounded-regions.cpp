989d6a0c879244d013ae657eb8d138016d2f584c
Alay <60166361+patelalay231@users.noreply.github.com>
2024-05-29 11:10:53 +0530

    Time: 8 ms (75.24%), Space: 14.9 MB (17.05%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    void dfs(vector<vector<char>>& board,int rows,int cols ,int i, int j,vector<vector<int>>& isConnectedToEdge){
+
+        vector<pair<int,int>> dir = {{0,1},{1,0},{0,-1},{-1,0}};
+        
+        for(auto it:dir){
+            int ni = it.first + i;
+            int nj = it.second + j;
+
+            if(ni >=0 && ni < rows && nj >= 0 && nj < cols && !isConnectedToEdge[ni][nj] && board[ni][nj] == 'O'){
+                isConnectedToEdge[ni][nj] = 1;
+                dfs(board,rows,cols,ni,nj,isConnectedToEdge);
+            }
+        }
+
+    }
+
+    void solve(vector<vector<char>>& board) {
+        int rows = board.size();
+        int cols = board[0].size();
+        vector<vector<int>> isConnectedToEdge(rows,vector<int>(cols,0));
+
+        for(int i=0;i<rows;i++){
+            if(board[i][0] == 'O'){
+                dfs(board,rows,cols,i,0,isConnectedToEdge);
+            }
+        }
+
+        for(int i=0;i<cols;i++){
+            if(board[0][i] == 'O'){
+                dfs(board,rows,cols,0,i,isConnectedToEdge);
+            }
+        }
+
+        for(int i=0;i<rows;i++){
+            if(board[i][cols-1] == 'O'){
+                dfs(board,rows,cols,i,cols-1,isConnectedToEdge);
+            }
+        }
+
+        for(int i=0;i<cols;i++){
+            if(board[rows-1][i] == 'O'){
+                dfs(board,rows,cols,rows-1,i,isConnectedToEdge);
+            }
+        }
+
+        for(int i=1;i<rows-1;i++){
+            for(int j=1;j<cols-1;j++){
+                if(board[i][j] == 'O' && !isConnectedToEdge[i][j]){
+                    board[i][j] = 'X';
+                }
+            }
+        }
+
+    }
+};
