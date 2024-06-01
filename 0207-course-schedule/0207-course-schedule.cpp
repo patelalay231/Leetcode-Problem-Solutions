00a13ab35bcf3897f7081f1997852eb4eca2d6f0
Alay <60166361+patelalay231@users.noreply.github.com>
2024-06-01 12:42:35 +0530

    Time: 18 ms (43.76%), Space: 17.6 MB (45.92%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> inDegree(numCourses, 0);
 
         for (auto it : prerequisites) {
-            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
 
         for (int i = 0; i < numCourses; i++) {
