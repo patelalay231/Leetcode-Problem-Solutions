fdc5c069fdb79034a8edbd6f742cb16a090feb7b
Alay <60166361+patelalay231@users.noreply.github.com>
2024-06-12 22:38:27 +0530

    Time: 2 ms (49.21%), Space: 7.2 MB (50.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int climbStairs(int n) {
-       if(n <= 1) return 1;
        int prev = 1, prev2 = 1;
        for(int i=2;i<=n;i++){
         int curri = prev+ prev2;
