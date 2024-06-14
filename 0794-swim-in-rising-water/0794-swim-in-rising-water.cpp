a50d0463c6f788f1c894f996550c2a48425bf35b
Alay <60166361+patelalay231@users.noreply.github.com>
2024-06-14 16:39:52 +0530

    Time: 15 ms (54.04%), Space: 12.5 MB (62.09%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,34 +1,34 @@
-typedef pair<int,pair<int,int>> triplet;
+typedef pair<int, pair<int, int>> triplet;
+
 class Solution {
-private:
-    bool isValid(int r,int c,int rows,int cols,vector<vector<int>> vis){
-        return r >= 0 && r < rows && c >= 0 && c < cols && vis[r][c] != 1;
-    }
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
-        vector<vector<int>> vis(rows,vector<int> (cols,0));
-        priority_queue<triplet,vector<triplet>,greater<triplet>> pq;
-        pq.push({grid[0][0],{0,0}});
-        int rd[] = {-1,0,1,0};
-        int cd[] = {0,1,0,-1};
+        vector<vector<bool>> vis(rows, vector<bool>(cols, false));
+        priority_queue<triplet, vector<triplet>, greater<triplet>> pq;
+        pq.push({grid[0][0], {0, 0}});
+        int rd[] = {-1, 0, 1, 0};
+        int cd[] = {0, 1, 0, -1};
 
-        while(!pq.empty()){
+        while (!pq.empty()) {
             int depth = pq.top().first;
             int r = pq.top().second.first;
             int c = pq.top().second.second;
             pq.pop();
-            if(r == rows-1 && c == cols-1){
+
+            if (r == rows - 1 && c == cols - 1) {
                 return depth;
             }
+
             if (vis[r][c]) continue;
-            vis[r][c] = 1;
-            for(int i=0;i<4;i++){
-                int nr = rd[i] + r;
-                int nc = cd[i] + c;
-                if(isValid(nr,nc,rows,cols,vis)){
-                    pq.push({max(grid[nr][nc],depth),{nr,nc}});
+            vis[r][c] = true;
+
+            for (int i = 0; i < 4; ++i) {
+                int nr = r + rd[i];
+                int nc = c + cd[i];
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && !vis[nr][nc]) {
+                    pq.push({max(grid[nr][nc], depth), {nr, nc}});
                 }
             }
         }
