9a9164e0b9017516d68a34c6e24f8dc641ffe024
Alay <60166361+patelalay231@users.noreply.github.com>
2024-06-14 19:30:13 +0530

    Time: 0 ms (100%), Space: 9.3 MB (10.57%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(),-1);
+        dp[0] = nums[0];
+        for(int i=1;i<nums.size();i++){
+            int pick = nums[i];
+            if(i>1) pick += dp[i-2];
+            int non_pick = dp[i-1];
+            dp[i] = max(pick,non_pick);
+        }
+        return dp[nums.size()-1];
+    }
+};
