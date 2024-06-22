0ff33337424bf98973f9b66c2fb005dd9297e79e
Alay <60166361+patelalay231@users.noreply.github.com>
2024-06-22 12:28:52 +0530

    Time: 223 ms (36.53%), Space: 182.7 MB (11.99%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -15,6 +15,7 @@ public:
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(),nums.end(),0);
         int n = nums.size();
+        if(sum%2 != 0) return false;
         vector<vector<int>> dp(n,vector<int>(sum,-1));
         return helper(nums,0,0,sum,dp);
     }
