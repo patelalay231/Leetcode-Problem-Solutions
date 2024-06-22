04279cb20a801274b0b7d2267494ad40b19585df
Alay <60166361+patelalay231@users.noreply.github.com>
2024-06-22 12:22:38 +0530

    Time: 352 ms (9.5%), Space: 240.9 MB (5.83%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool helper(vector<int>& nums,int i,int sum,int rem,vector<vector<int>>& dp){
+        if(sum == rem){
+            return true;
+        }
+        if(i == nums.size()-1){
+            return sum == rem;
+        }
+        if(dp[i][sum] != -1) return dp[i][sum];
+        bool take = helper(nums,i+1,sum+nums[i],rem-nums[i],dp);
+        bool notTake = helper(nums,i+1,sum,rem,dp);
+        return dp[i][sum] = take || notTake;
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        int n = nums.size();
+        vector<vector<int>> dp(n,vector<int>(sum,-1));
+        return helper(nums,0,0,sum,dp);
+    }
+};
