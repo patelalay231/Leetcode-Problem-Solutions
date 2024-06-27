8a9474d86daa78cfa68fc09be98ea3aca9d64af5
Alay <60166361+patelalay231@users.noreply.github.com>
2024-06-27 23:21:46 +0530

    Time: 43 ms (17.45%), Space: 25.8 MB (33.09%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    //     int f(int  i , int j, string &text1, string &text2
-    //     ,vector<vector<int>> &dp ){
-    //         if( i < 0 || j < 0 )
-    //         return 0;
+    int lcs(int idx1, int idx2, string& text1, string& text2,
+            vector<vector<int>>& dp) {
+        if (idx1 < 0 || idx2 < 0)
+            return 0;
+        if (dp[idx1][idx2] != -1)
+            return dp[idx1][idx2];
+        if (text1[idx1] == text2[idx2])
+            return dp[idx1][idx2] =
+                       1 + lcs(idx1 - 1, idx2 - 1, text1, text2, dp);
 
-    // if(text1[i] == text2[j])
-    //  return dp[i][j] = 1 + f(i-1,j-1,text1 , text2 , dp);
-    // return dp[i][j] = max(f(i-1,j,text1 , text2 , dp) , f(i,j-1,text1 , text2
-    // , dp));
-
-    //     }
+        return dp[idx1][idx2] = max(lcs(idx1 - 1, idx2, text1, text2, dp),
+                                    lcs(idx1, idx2 - 1, text1, text2, dp));
+    }
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        // base
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = 0;
-        for (int j = 0; j <= m; j++)
-            dp[0][j] = 0;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-
-                if (text1[i - 1] == text2[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-
-        return dp[n][m];
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return lcs(n - 1, m - 1, text1, text2, dp);
     }
 };
