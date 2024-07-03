1ffe1dfa61134186e25097c08335d538c67c2249
Alay <60166361+patelalay231@users.noreply.github.com>
2024-07-03 16:05:08 +0530

    Time: 72 ms (46.36%), Space: 74.2 MB (44.04%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
-    int lcs(int idx1, int idx2, string& text1, string& text2,
-            vector<vector<int>>& dp) {
-        if (idx1 < 0 || idx2 < 0)
-            return 0;
-        if (dp[idx1][idx2] != -1)
-            return dp[idx1][idx2];
-        if (text1[idx1] == text2[idx2])
-            return dp[idx1][idx2] =
-                       1 + lcs(idx1 - 1, idx2 - 1, text1, text2, dp);
+    int longestCommonSubsequence(string s1, string s2) {
 
-        return dp[idx1][idx2] = max(lcs(idx1 - 1, idx2, text1, text2, dp),
-                                    lcs(idx1, idx2 - 1, text1, text2, dp));
+        int n = s1.size();
+        int m = s2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s1[ind1 - 1] == s2[ind2 - 1])
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                else
+                    dp[ind1][ind2] =
+                        max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+            }
+        }
+        return dp[n][m];
     }
     int longestPalindromeSubseq(string s) {
-        string revS = s;
-        reverse(revS.begin(), revS.end());
-        int n = s.size();
-        int m = revS.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return lcs(n - 1, m - 1, s, revS, dp);
+
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        return longestCommonSubsequence(s, s2);
     }
+
 };
