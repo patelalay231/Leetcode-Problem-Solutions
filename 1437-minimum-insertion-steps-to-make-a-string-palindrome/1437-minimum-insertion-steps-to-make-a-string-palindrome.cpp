0b7d652bc49f45206e0ac93d3e082ffda3a153ed
Alay <60166361+patelalay231@users.noreply.github.com>
2024-07-04 20:24:25 +0530

    Time: 26 ms (68.5%), Space: 8 MB (92.25%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int lcs(string& s1,string& s2,int idx1,int idx2,vector<vector<int>>& dp){
-        if(idx1 < 0 || idx2 < 0){
-            return 0;
-        }
-        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
-        if(s1[idx1] == s2[idx2]){
-            dp[idx1][idx2] = 1 + lcs(s1,s2,idx1-1,idx2-1,dp);
-            return dp[idx1][idx2];
-        }
-        return dp[idx1][idx2] = max(lcs(s1,s2,idx1-1,idx2,dp),lcs(s1,s2,idx1,idx2-1,dp));
-    }
     int minInsertions(string s) {
-        string revS = s;
-        reverse(s.begin(),s.end());
-        int len = s.length();
-        vector<vector<int>> dp(len,vector<int>(len,-1));
-        return len - lcs(s,revS,len-1,len-1,dp);
+        // n-len(l c palindromic subseq).
+        int n=s.size();
+        string s1=s;
+        reverse(s.begin(), s.end());
+        string s2=s;
+        vector<int> prev(n+1, 0), curr(n+1, 0);
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=n; j++){
+                if (s1[i-1]==s2[j-1]){
+                    curr[j]=prev[j-1]+1;
+                }else{
+                    curr[j]=max(curr[j-1], prev[j]);
+                }
+            }prev=curr;
+        }
+        int palin=prev[n];
+        int ans=n-palin;
+        return ans;
     }
 };
