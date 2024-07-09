3f8b1b05f11ebb910da25da39199c847729e8a41
Alay <60166361+patelalay231@users.noreply.github.com>
2024-07-09 13:29:35 +0530

    Time: 1683 ms (20.45%), Space: 640 MB (9.22%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,42 +1,63 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    static bool cmp(const string& a, const string& b) {
-        return a.size() < b.size();
-    }
 
-    bool checkSubseq(string a, string b) {
-        int n = a.size();
-        int m = b.size();
-        if (n != m - 1) {
-            return false;
-        }
-        int i = 0, j = 0;
-        while (i < n && j < m) {
-            if (a[i] == b[j]) {
-                i++;
+    static bool compare(string &s1,string &s2){
+        return s1.size() < s2.size();
+    }  
+
+    bool isPredecessor(string s1, string s2){
+
+        if(s1.length() + 1 == s2.length()){
+
+            int count = 0;
+            int k = 0;
+            char c = s1[0];
+            for(int i = 0; i<s2.length(); i++){
+
+                if(s2[i] == c){
+                    c = s1[++k];
+                }else{
+                    count++;
+                    if(count>1){
+                        return false;
+                    }
+                }
+
             }
-            j++;
+            return true;
+
         }
-        return i == n;
-    }
+
+        return false;
+
+    } 
 
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(), words.end(), cmp);
-        int n = words.size();
-        vector<int> dp(n,1);
-        int maxi = 1;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(checkSubseq(words[j],words[i]) && dp[j]+1 > dp[i]){
-                    dp[i] = dp[j] + 1;
+     
+        sort(words.begin(), words.end(), compare);
+
+        int start = 0;
+        int end = 1;
+        int maxCount = 1;
+
+        map<int, int> m;
+
+        for(int i = 0;i<words.size(); i++){
+            int count = m[i];
+            start = i;
+            for(int end = i+1; end<words.size(); end++){
+
+                if(isPredecessor(words[start], words[end])){
+                    m[end] = max(count + 1, m[end]);
                 }
+
             }
-            maxi = max(maxi,dp[i]);
+
+            maxCount = max(maxCount, count + 1);
+
         }
+
+        return maxCount;
         
-        return maxi; 
     }
 };
