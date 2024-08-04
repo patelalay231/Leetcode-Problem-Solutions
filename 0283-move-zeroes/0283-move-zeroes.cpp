5e3765a032abc22ec324e05cab1db2c9148a7421
Alay <60166361+patelalay231@users.noreply.github.com>
2024-08-05 02:21:09 +0530

    Time: 23 ms (16.88%), Space: 21.9 MB (18%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int j=0,n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                swap(arr[j++],arr[i]);
+            }
+        } 
+    }
+};
