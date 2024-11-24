2472c43701cdafac8162c05d948ba3a49df69c61
Alay <60166361+patelalay231@users.noreply.github.com>
2024-11-24 23:00:58 +0530

    Time: 5 ms (8.06%), Space: 11.5 MB (10.02%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    int helper(string s, int n, int i,vector<int>& dp) {
-        if (n == i) {
+    int helper(string s, int n, int i, vector<int>& dp) {
+        if (i == n) {
             return 1; // Base case: reached the end of the string
         }
-        if(dp[i] != -1) return dp[i];
-        int one = 0, two = 0;
-        if (s[i] == '0') {
-            return 0; // Leading zero is not valid
-        }
-        // Handle single-digit case
-        if (s[i] - '0' < 27 && s[i] - '0' > 0) {
-            one += helper(s, n, i + 1,dp);
-        }
+        if (dp[i] != -1) return dp[i];
+        if (s[i] == '0') return 0; // Leading zero is not valid
 
-        // Handle two-digit case
-        if (i + 1 < n) {
+        int one = helper(s, n, i + 1, dp); // Single digit decode
+        int two = 0;
+
+        if (i + 1 < n) { // Two-digit decode
             int sub = stoi(s.substr(i, 2));
-            if (sub < 27 && sub > 0) {
-                two += helper(s, n, i + 2,dp);
+            if (sub <= 26) {
+                two = helper(s, n, i + 2, dp);
             }
         }
 
-        dp[i] = one + two;
-        return dp[i];
+        return dp[i] = one + two;
     }
+
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n,-1);
-        return helper(s, n, 0,dp);
+        vector<int> dp(n, -1);
+        return helper(s, n, 0, dp);
     }
 };
