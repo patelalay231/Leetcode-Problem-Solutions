fb1cd7bd30bfc9d40cc6df4500b5bc130b34e444
Alay <60166361+patelalay231@users.noreply.github.com>
2024-12-05 19:58:58 +0530

    Time: 0 ms (100%), Space: 40.2 MB (34.76%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,33 +2,16 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        int mul = 1;  // To store the product of non-zero elements
-        int zeros = 0;  // Count of zeros in the array
-        
-        // Step 1: Calculate the product of all non-zero elements and count zeros
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) 
-                zeros++;
-            else 
-                mul *= nums[i];
+        vector<int> ans(n,1);
+        int pre=nums[0],pos=nums[n-1];
+        for(int i=1;i<n;i++){
+            ans[i] = pre;
+            pre = pre * nums[i];
         }
-        
-        vector<int> ans(n, 0); 
-        if (zeros > 1) {
-            
-            return ans;
+        for(int i=n-2;i>=0;i--){
+            ans[i] = ans[i] * pos;
+            pos = pos * nums[i];
         }
-        
-        for (int i = 0; i < n; i++) {
-            if (zeros == 1) {
-                if (nums[i] == 0) {
-                    ans[i] = mul;
-                }
-            } else {
-                ans[i] = mul / nums[i];
-            }
-        }
-        
         return ans;
     }
 };
