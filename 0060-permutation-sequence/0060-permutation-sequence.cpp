762bd5283715463278e74a20301a17620e004b92
Alay <60166361+patelalay231@users.noreply.github.com>
2024-12-17 14:54:08 +0530

    Time: 0 ms (100%), Space: 8.9 MB (10.57%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int stop_point = -1;
-        /* finding breaking point where condition fails that right side of
-        that ele is smaller than that element */
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                stop_point = i;
+    string getPermutation(int n, int k) {
+        int fact=1;
+        vector<int> nums;
+        for(int i=1;i<n;i++){
+            fact=fact*i;
+            nums.push_back(i);
+        }
+        nums.push_back(n);
+        k=k-1;
+        string ans="";
+        while(true){
+            ans=ans+to_string(nums[k/fact]);
+            nums.erase(nums.begin()+k/fact);
+            if(nums.size()==0){
                 break;
             }
-        }
-        if (stop_point == -1) {
-            // reverse the whole array:
-            reverse(nums.begin(), nums.end());
-        } else {
-            for (int i = n - 1; i > stop_point; i--) {
-                if (nums[i] > nums[stop_point]) {
-                    swap(nums[i], nums[stop_point]);
-                    break;
-                }
-            }
-            reverse(nums.begin() + stop_point + 1, nums.end());
-        }
-    }
-    string getPermutation(int n, int k) {
-        vector<int> nums(n,0);
-        for(int i=0;i<n;i++){
-            nums[i] = i+1;
-        }
-        for(int i=0;i<k-1;i++){
-            nextPermutation(nums);
-        }
-        string ans = "";
-        for(auto it : nums){
-            ans += to_string(it);
+            k=k%fact;
+            fact=fact/nums.size();
         }
         return ans;
     }
