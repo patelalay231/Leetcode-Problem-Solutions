8678a30b01291eeef7be44094da8a183c092c081
Alay <60166361+patelalay231@users.noreply.github.com>
2025-03-08 01:01:23 +0530

    Time: 0 ms (100%), Space: 23.7 MB (96.88%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& arr) {
-        int j=0,n=arr.size();
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int zero_idx = -1;
         for(int i=0;i<n;i++){
-            if(arr[i]!=0){
-                swap(arr[j++],arr[i]);
+            if(nums[i] != 0 && zero_idx != -1){
+                swap(nums[i],nums[zero_idx]);
+                zero_idx++;
             }
-        } 
-    }
+            else if(nums[i] == 0 && zero_idx == -1){
+                zero_idx = i;
+            }
+        }
+    }   
 };
